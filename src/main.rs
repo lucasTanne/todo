8adfb327d10a482eeadbd8d4927274a60ab0694f
Lucas Tanne <lucas.tanne29@gmail.com>
2026-03-07 23:45:29 +0100

    rework

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,67 @@
-mod todo_list;
+use std::io;
+use uuid::Uuid;
+
+struct Task {
+    uuid: Uuid,
+    name: String,
+    description: String
+}
+
+struct TodoList {
+    tasks: Vec<Task>
+}
+
+impl TodoList {
+    /// Creates a new task using given information and append it into its tasks list.
+    fn new_task(&mut self, name: String, description: String) {
+        let task: Task = Task{
+            uuid: Uuid::new_v4(),
+            name: name,
+            description: description,
+        };
+        self.tasks.push(task);
+    }
+
+    /// returns a task identified by the given UUID
+    fn get_task(&self, uuid: &Uuid) -> Option<&Task> {
+        let pos = self.tasks.iter().position(|t| t.uuid.eq(uuid));
+
+        if pos.is_some() {
+            return self.tasks.get(pos.unwrap());
+        }
+
+        return None;
+    }
+
+    /// Removes a task identified by its UUID
+    fn delete_task(&mut self, uuid: &Uuid) -> Result<usize, String> {
+        let pos = self.tasks.iter().position(|t| t.uuid.eq(uuid));
+        
+        if pos.is_some() {
+            let idx: usize = pos.unwrap();
+            self.tasks.remove(idx);
+            return Ok(idx);
+        }
+
+        return Err(String::from("not found"));
+    }
+}
 
 fn main() {
-    let mut todo_list: todo_list::TodoList = todo_list::TodoList::new();
+    let mut todo_list: TodoList = TodoList{ 
+        tasks: Vec::new()
+    };
 
-    todo_list.add_task("Task one", "The first task should be to test this Todo list ");
+    let mut name: String = String::new();
+    println!("Name of the task: ");
+    io::stdin().read_line(&mut name).expect("Could not read name from stdin");
 
-    todo_list.add_list("home");
-    todo_list.add_task_to_list("home", "Task two", "Refactor my brain using rust !");
-    // todo_list.add_task_to_list("taf", "Task three", "Err task");
+    let mut description: String = String::new();
+    println!("Description of the task: ");
+    io::stdin().read_line(&mut description).expect("Could nto read description from stdin");
 
-    todo_list.print_todo_list();
+    todo_list.new_task(name, description);
+
+    println!("Task len: {}", todo_list.tasks.len());
+
 }
